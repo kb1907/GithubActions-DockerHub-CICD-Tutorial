a10ac39c6f5924a3bd16fa54595f5035bd42c01b
KB <kaanboke@gmail.com>
2022-08-30 21:53:05 +0300

    requirements file and YAML file are updated.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 app = Flask(__name__)
-model = pickle.load(open('catboost_model-2.pkl', 'rb'))
+model = pickle.load(open("catboost_model-2.pkl", "rb"))
 
 
 def model_pred(features):
@@ -13,32 +13,38 @@ def model_pred(features):
     return int(prediction[0])
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def Home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    if request.method == 'POST':
-        Age = int(request.form['Age'])
-        RestingBP = int(request.form['RestingBP'])
-        Cholesterol = int(request.form['Cholesterol'])
-        Oldpeak = float(request.form['Oldpeak'])
-        FastingBS = int(request.form['FastingBS'])
-        MaxHR = int(request.form['MaxHR'])
-        prediction = model.predict([[Age, RestingBP, Cholesterol, FastingBS, MaxHR, Oldpeak
-                                     ]])
+    if request.method == "POST":
+        Age = int(request.form["Age"])
+        RestingBP = int(request.form["RestingBP"])
+        Cholesterol = int(request.form["Cholesterol"])
+        Oldpeak = float(request.form["Oldpeak"])
+        FastingBS = int(request.form["FastingBS"])
+        MaxHR = int(request.form["MaxHR"])
+        prediction = model.predict(
+            [[Age, RestingBP, Cholesterol, FastingBS, MaxHR, Oldpeak]]
+        )
 
         if prediction[0] == 1:
-            return render_template('index.html', prediction_text="Kindly make an appointment with the doctor!")
+            return render_template(
+                "index.html",
+                prediction_text="Kindly make an appointment with the doctor!",
+            )
 
         else:
-            return render_template('index.html', prediction_text="You are well. No worries :)")
+            return render_template(
+                "index.html", prediction_text="You are well. No worries :)"
+            )
 
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
